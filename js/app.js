09b4018f659e499493d53aae0d4c9d0ab428a4a6
Boris <borissicsac@gmail.com>
2021-03-02 13:27:08 -0300

    actualizacion app.js
    desarrollo de todas las funciones excepto el almacenamiento local.

--- js/app.js
+++ js/app.js
@@ -9,6 +9,10 @@ const CHECK = "fa-check-circle";
 const UNCHECK = "fa-circle-thin";
 const LINE_THROUGH = "lineThrough";
 
+//Variables
+let LIST = []
+    , id = 0;
+
 //Fecha actual
 const options = {weekday : "long", month : "short", day : "numeric"};
 const today = new Date();
@@ -16,12 +20,17 @@ const today = new Date();
 dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
 //añadir funcion ToDo
-function addToDo(toDo){
+function addToDo(toDo, id, done, trash){
+
+    if(trash){return;}
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH : "";
+
     const item = `
                 <li class="item">
-                    <i class="fa fa-circle-thin co" job="complete" id="0"></i>
-                    <p class="text">${toDo}</p>
-                    <i class="fa fa-trash-o de" job="delete" id="0"></i>
+                    <i class="fa ${DONE} co" job="complete" id="${id}"></i>
+                    <p class="text ${LINE}">${toDo}</p>
+                    <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
                 </li>
                 `;
     const position = "beforeend";
@@ -32,3 +41,66 @@ function addToDo(toDo){
 //addToDo("Play Spotify");
 
 //Añadir item a la listaa cuando el usuario presiona ENTER
+document.addEventListener("keyup",function(even)
+    {
+        if(event.keyCode == 13)
+        {
+            const toDo = input.value;
+
+            //Cuando el input no esta vacio
+            if(toDo)
+            {
+                addToDo(toDo, id, false, false);
+                LIST.push({
+                    name : toDo, 
+                    id : id, 
+                    done : false, 
+                    trash : false
+                });
+                id++;
+            }
+            input.value = "";
+        }
+    }
+);
+
+//Prueba con ToDo finalizado
+//addToDo("Play Youtube", 1, true, false);
+
+//Completar ToDo
+function completeToDo(element)
+{
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+
+    LIST[element.id].odne = LIST[element.id].done ? false : true;
+}
+
+//Remover ToDo
+function removeToDo(element)
+{
+    element.parentNode.parentNode.removeChild(element.parentNode);
+    LIST[element.id].trash = true;
+}
+
+//Seleccionar Items creados dinamicamente
+list.addEventListener("click", function(event)
+    {
+        //Devuelve el elemento clickeado dentro de la lista
+        const element = event.target;
+
+        //Completar o borrar
+        const elementJob = element.attributes.job.value;
+
+        if(elementJob == "complete")
+        {
+            completeToDo(element);
+        }
+        else if (elementJob == "delete")
+        {
+            removeToDo(element);
+        }
+    }
+
+);
